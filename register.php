18d6919cf497034f7a8648db800430128553f438
2GreekDevs <2greekdevs24@gmail.com>
2025-08-03 02:52:16 +0300

    Create register.php

---
+++ register.php
@@ -0,0 +1,17 @@
+<?php
+require 'db.php';
+
+$username = $_POST['username'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+$sql = "INSERT INTO users (username, email, password) VALUES (?, ?, ?)";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("sss", $username, $email, $password);
+
+if ($stmt->execute()) {
+    header("Location: index.html");
+} else {
+    echo "Error: " . $conn->error;
+}
+?>
